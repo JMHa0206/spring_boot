efa3e4cea60f58c475d59c2f0ff4b5afd0d7cd94
tester <cdcd718@naver.com>
2025-04-04 17:49:29 +0900

    board springboot

--- src/main/java/com/kedu/study/dao/BoardDAO.java
+++ src/main/java/com/kedu/study/dao/BoardDAO.java
@@ -1,5 +1,31 @@
 package com.kedu.study.dao;
 
-public class BoardDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kedu.study.dto.BoardDTO;
+import com.kedu.study.dto.ContactsDTO;
+
+@Repository
+public class BoardDAO {
+	@Autowired
+	public SqlSession mybatis;
+	
+	public List<ContactsDTO> selectAll(){
+		System.out.println("3");
+		return mybatis.selectList("Contacts.selectAll");
+	};
+	
+	public int insert(ContactsDTO dto) {
+		return mybatis.insert("Contacts.insertContact",dto);
+		
+	}
+	//아이디로 삭제
+	public int delete(int id) {
+		return mybatis.delete("Contacts.deleteContact",id);
+	}
 }
