e42605b1e862c364d39ea703a554fec5f323a3a9
you@example.com <you@example.com>
2025-04-07 15:12:26 +0900

    1대1 채팅 기능 구현

--- src/main/java/com/kedu/study/dao/MessageDAO.java
+++ src/main/java/com/kedu/study/dao/MessageDAO.java
@@ -1,5 +1,19 @@
 package com.kedu.study.dao;
 
-public class MessageDAO {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.kedu.study.dto.MessageDTO;
+
+@Repository
+public class MessageDAO {
+	
+	@Autowired
+	private SqlSession mybatis;
+	
+	public void saveMessage(MessageDTO message) {
+		mybatis.insert("Message.saveMessage",message);
+	}
+	
 }
