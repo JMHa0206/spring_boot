5a6ee31dea2b170f42b49b558525abfcec83468a
tester <cdcd718@naver.com>
2025-04-08 18:15:41 +0900

    rjirrjgr

--- src/main/java/com/kedu/study/service/BoardService.java
+++ src/main/java/com/kedu/study/service/BoardService.java
@@ -14,11 +14,27 @@ public class BoardService {
 	
 	  @Autowired
 	    public BoardDAO bdao;
-
-	    public List<BoardDTO> getBoardList() {
-	    	  System.out.println("2"); 
-	        return bdao.selectAll();
+	  
+	  	//테이블 전체를 불러오는 부분
+	    public List<BoardDTO> selectAll() {
+	    	List<BoardDTO> list = bdao.selectAll();
+	        return list;
+	    }
+	    
+	    //board_id로 제목이랑 내용불러오는 부분
+	    public BoardDTO getBoardById(int board_id) {
+	   	  System.out.println("2"); 
+	    	return bdao.findBoardid(board_id);
 	    }
 
-
+	    //수정부분
+	    public void updateBoard(BoardDTO board) {
+	    	bdao.updateBoard(board);
+	    }
+	    
+	    //삭제부분
+	    public void deleteBoard(int board_id) {
+	    	bdao.deleteBoard(board_id);
+	    }
+	    
 }
