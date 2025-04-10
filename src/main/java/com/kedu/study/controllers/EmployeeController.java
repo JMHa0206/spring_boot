9ac4b85fd2f3bb58fb0d65c746f3295aa87e4bc3
HJM <jungmzzang@naver.com>
2025-04-10 14:41:03 +0900

    공유캘린더 구현 중

--- src/main/java/com/kedu/study/controllers/EmployeeController.java
+++ src/main/java/com/kedu/study/controllers/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.kedu.study.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kedu.study.dto.DepartDTO;
+import com.kedu.study.dto.EmployeeDTO;
 import com.kedu.study.service.EmployeeService;
 
 @RestController
@@ -12,5 +18,17 @@ public class EmployeeController {
 	
 	@Autowired
 	private EmployeeService eServ;
+	
+	@GetMapping("/selectAllEmps")
+	public ResponseEntity<List<EmployeeDTO>> selectAllEmps(){
+		List<EmployeeDTO> empList = eServ.selectAllEmps();
+		return ResponseEntity.ok(empList);
+	}
+	
+	@GetMapping("/selectAllDepts")
+	public ResponseEntity<List<DepartDTO>> selectAllDepts(){
+		List<DepartDTO> deptList = eServ.selectAllDepts();
+		return ResponseEntity.ok(deptList);
+	}
 
 }
--- src/main/java/com/kedu/study/service/EmployeeService.java
+++ src/main/java/com/kedu/study/service/EmployeeService.java
@@ -1,14 +1,26 @@
 package com.kedu.study.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kedu.study.dao.EmployeeDAO;
+import com.kedu.study.dto.DepartDTO;
+import com.kedu.study.dto.EmployeeDTO;
 
 @Service
 public class EmployeeService {
 	
 	@Autowired
 	private EmployeeDAO eDao;
+	
+	public List<EmployeeDTO> selectAllEmps(){
+		return eDao.selectAllEmps();
+	}
+	
+	public List<DepartDTO> selectAllDepts(){
+		return eDao.selectAllDepts();
+	}
 
 }
