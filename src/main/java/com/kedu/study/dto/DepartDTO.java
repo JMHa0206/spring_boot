9ba10c49331c36647e108dc1f31b6f518000f7bd
Xavier Choi <chaeins25@naver.com>
2025-04-11 17:37:45 +0900

    fixed for EDMS issue

--- src/main/java/com/kedu/study/dto/DepartDTO.java
+++ src/main/java/com/kedu/study/dto/DepartDTO.java
@@ -4,17 +4,31 @@ public class DepartDTO {
 	private Integer dept_id;
 	private String dept_name;
 	private Integer dept_manager; // updated
+	private Integer upper_dept;
 	private String emp_name;
 
 	// 기본 생성자
 	public DepartDTO() {}
-
-	public DepartDTO(Integer dept_id, String dept_name, Integer dept_manager, String emp_name) {
+	
+	public DepartDTO(Integer dept_id, String dept_name, Integer dept_manager, Integer upper_dept, String emp_name) {
 		super();
 		this.dept_id = dept_id;
 		this.dept_name = dept_name;
 		this.dept_manager = dept_manager;
+		this.upper_dept = upper_dept;
 		this.emp_name = emp_name;
+	}
+
+
+
+	public Integer getUpper_dept() {
+		return upper_dept;
+	}
+
+
+
+	public void setUpper_dept(Integer upper_dept) {
+		this.upper_dept = upper_dept;
 	}
 
 	public Integer getDept_id() {
