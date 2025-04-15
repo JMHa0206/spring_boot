c1c27e235e0395bda69e1a5437fb7861664f0f49
jeokaet <whgnldud0325@naver.com>
2025-04-15 17:03:59 +0900

    test1

---
+++ src/main/java/com/kedu/study/dto/Departsimple.java
@@ -0,0 +1,27 @@
+package com.kedu.study.dto;
+
+public class Departsimple {
+	private Integer dept_id;
+    private String dept_name;
+    
+    public Departsimple() {}
+    
+	public Departsimple(Integer dept_id, String dept_name) {
+		super();
+		this.dept_id = dept_id;
+		this.dept_name = dept_name;
+	}
+	
+	public Integer getDept_id() {
+		return dept_id;
+	}
+	public void setDept_id(Integer dept_id) {
+		this.dept_id = dept_id;
+	}
+	public String getDept_name() {
+		return dept_name;
+	}
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
+	}
+}
